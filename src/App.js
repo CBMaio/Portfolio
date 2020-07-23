a5561c9257c1d7c9d031295a759ddcdc9f7129fa
CBMaio <carolinabelenmaio@gmail.com>
2020-07-23 12:04:43 -0300

    Importación de Switch, Router y Route para enlace interno entre componentes, e importamos nuevo componente de descripción personal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Main from './Componentes/Main'
-import Profile from './Componentes/Section-about-me';
+import SectionSkills from './Componentes/Section-habilidades';
+import AboutMeBox from './Componentes/AboutMe';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  
   return (
-    <div>
-      <Main/>
-      <Profile/>
-    </div>
-  );
+  
+       <Router>
+         <Switch>
+             <Route path="/" exact component={Main}/>
+             <Route path="/skills" component={SectionSkills}/>
+             <Route path="/aboutMe" component={AboutMeBox}/>
+         </Switch>
+       </Router>
+   );
 }
 
 export default App;
