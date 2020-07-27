3d6cd96bfd6acc8ae3a35403b5898a749cee266e
CBMaio <carolinabelenmaio@gmail.com>
2020-07-27 10:58:04 -0300

    Updates

---
+++ precache-manifest.befffb6cf73f9a45c09cc4b6aee2cb6b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149f9c52b8ee15e716c73d4bba0116e5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fb4b7c6206748cdf3c38",
+    "url": "/Portfolio/static/css/main.891ed3c1.chunk.css"
+  },
+  {
+    "revision": "8a157198fa6c0bfe819a",
+    "url": "/Portfolio/static/js/2.106a099e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.106a099e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4b7c6206748cdf3c38",
+    "url": "/Portfolio/static/js/main.44085016.chunk.js"
+  },
+  {
+    "revision": "9bb5a14c4411ad556432",
+    "url": "/Portfolio/static/js/runtime-main.d985277c.js"
+  },
+  {
+    "revision": "6845c46ec11ef9c78cd93afac66bdbab",
+    "url": "/Portfolio/static/media/foto-Reverse.6845c46e.png"
+  },
+  {
+    "revision": "3e55902754bfea307dddfe906fc1e892",
+    "url": "/Portfolio/static/media/icomoonBtn.3e559027.ttf"
+  },
+  {
+    "revision": "3f07cee9ed6de7db60d968e5cb77c11d",
+    "url": "/Portfolio/static/media/icomoonBtn.3f07cee9.woff"
+  },
+  {
+    "revision": "8fb656be8f11bbc1464e5bdffd0d89c0",
+    "url": "/Portfolio/static/media/icomoonBtn.8fb656be.svg"
+  },
+  {
+    "revision": "b1c824833e0dd3ebc80ff8742107906b",
+    "url": "/Portfolio/static/media/icomoonBtn.b1c82483.ttf"
+  },
+  {
+    "revision": "ce9dd5eb9d54124199b14fce5a9e3832",
+    "url": "/Portfolio/static/media/icomoonBtn.ce9dd5eb.eot"
+  },
+  {
+    "revision": "d7c3e32e54a9878eb6f99dc729b87760",
+    "url": "/Portfolio/static/media/icomoonBtn.d7c3e32e.svg"
+  },
+  {
+    "revision": "f00fe1bbfad95b99d1256457ad6c25a8",
+    "url": "/Portfolio/static/media/icomoonBtn.f00fe1bb.woff"
+  },
+  {
+    "revision": "fe6d0117211d0b43559a3f63a61dea0a",
+    "url": "/Portfolio/static/media/icomoonBtn.fe6d0117.eot"
+  },
+  {
+    "revision": "da20fbe5f63b7f029b6c9a0bd70baf1a",
+    "url": "/Portfolio/static/media/image.da20fbe5.png"
+  }
+]);
